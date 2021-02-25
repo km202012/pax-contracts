e26a9e0681b797f370455c2ff4b8acd65cfb7f23
block2block <76591356+km202012@users.noreply.github.com>
2021-02-26 08:16:19 +0900

    Update Migrations.sol

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 pragma experimental "v0.5.0";
 
 
-// This is used for truffle migrations / testing.
+// mmThis is used for truffle migrations / testing.
 
 contract Migrations {
     uint public last_completed_migration;
